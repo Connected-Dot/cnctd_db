e134aa3a684c4b1f6d89dec61c71f7bac7b676fa
kebtech <64436886+kebtech@users.noreply.github.com>
2024-04-11 21:02:02 -0400

    Add sqlx, anyhow, state, and tokio dependencies to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use sqlx::Pool;
+use state::InitCell;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub static POSTGRES_POOL: InitCell<Pool<sqlx::Postgres>> = InitCell::new();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug)]
+pub struct CnctdDB;
+
+impl CnctdDB {
+    pub async fn new_pool(url: &str) -> Result<Pool<sqlx::Postgres>, sqlx::Error> {
+        let pool = sqlx::postgres::PgPoolOptions::new()
+            .max_connections(5)
+            .connect(url)
+            .await
+            ?;
+
+        Ok(pool)
     }
+    
+    pub async fn start(url: &str) -> anyhow::Result<()> {
+        let pool = Self::new_pool(url).await?;
+        POSTGRES_POOL.set(pool);
+
+        Ok(())
+    }
+
+    pub fn get_pool() -> anyhow::Result<&'static Pool<sqlx::Postgres>> {
+        Ok(POSTGRES_POOL.get())
+    }
+
+    
 }
